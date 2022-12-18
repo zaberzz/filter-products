af7d2291d57adee066a1864be742059f149557ff
Saba <saba.sadegh67@gmail.com>
2022-12-18 11:11:24 +0330

    display product

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('filters project');
+let filteredProduct = [...products];
+
+const producContainer = document.querySelector(".products-container");
+
+const displayProduct = () => {
+  producContainer.innerHTML = filteredProduct
+    .map(({ id, title, image, price }) => {
+      return `  <article class="product" data-id="${id}">
+    <img
+      src="${image}"
+      class="product-img img"
+    />
+    <footer>
+      <h5 class="product-name">${title}</h5>
+      <span class="product-price">$ ${price}</span>
+    </footer>
+  </article>`;
+    })
+    .join("");
+};
+displayProduct();
